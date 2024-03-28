63f046ba12912179958f8e4520c75192e73c167b
foolish-pine <matsuda.takuya41@gmail.com>
2024-03-28 21:08:37 +0900

    chore: format

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,6 +1,6 @@
 module.exports = {
-  plugins: [
-    require("postcss-preset-env"),
-    require("postcss-sort-media-queries"),
-  ],
+  plugins: {
+    "postcss-preset-env": {},
+    "postcss-sort-media-queries": {},
+  },
 };
